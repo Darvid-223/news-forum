5675e51e56e14b6d5778ae1e8ad6066ce1acc8a1
Darvid-223 <david.arvidsson@hotmail.com>
2024-08-29 20:50:51 +0200

    Catergory removed from urls

--- news/urls.py
+++ news/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('post/new/', views.post_create, name='post_create'),
     path('post/<int:id>/edit/', views.post_edit, name='post_edit'),
     path('post/<int:id>/delete/', views.post_delete, name='post_delete'),
-    path('category/<int:id>/', views.category_posts, name='category_posts'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'news/post_list.html', {'posts': posts})
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'news/post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user  
+            post.save()  
+            return redirect('post_detail', id=post.id)
+    else:
+        form = PostForm()
+    return render(request, 'news/post_form.html', {'form': form})
+
+
+def post_edit(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'news/post_form.html', {'form': form})
+
+
+def post_delete(request, id):
+    post = get_object_or_404(Post, id=id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'news/post_confirm_delete.html', {'post': post})
+
+
+
