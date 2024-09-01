d677c87cd308a829cefd9aae85810bc97b87767a
Darvid-223 <david.arvidsson@hotmail.com>
2024-09-01 19:21:50 +0200

    Changed styling with bvetter layout, google fonts and responsive design

--- news/forms.py
+++ news/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Post, Comment
+from .models import Post, Comment, Category
 
 
 class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        # pre select "News" category
+        self.fields['category'].initial = Category.objects.get(name="News")
+
     class Meta:
         model = Post
         fields = ['title', 'content', 'category']
