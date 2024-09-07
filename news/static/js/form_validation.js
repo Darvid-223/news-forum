92725007adccc36faf9929664c2afb26a3dabfc4
Darvid-223 <david.arvidsson@hotmail.com>
2024-09-07 14:58:57 +0200

    feat: add javascript form validation for post creation

---
+++ news/static/js/form_validation.js
@@ -0,0 +1,9 @@
+function validateForm() {
+    let title = document.forms["postForm"]["title"].value;
+    let content = document.forms["postForm"]["content"].value;
+    if (title === "" || content === "") {
+        alert("Both title and content must be filled out");
+        return false;
+    }
+    return true;
+}
