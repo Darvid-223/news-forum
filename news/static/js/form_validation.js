83933150ca246c3a0815ea6125692b5c42f4e70f
Darvid-223 <david.arvidsson@hotmail.com>
2024-09-07 15:03:14 +0200

    Refactor javascript to pass linting validation

--- news/static/js/form_validation.js
+++ news/static/js/form_validation.js
@@ -1,6 +1,6 @@
 function validateForm() {
-    let title = document.forms["postForm"]["title"].value;
-    let content = document.forms["postForm"]["content"].value;
+    let title = document.forms.postForm.title.value;
+    let content = document.forms.postForm.content.value;
     if (title === "" || content === "") {
         alert("Both title and content must be filled out");
         return false;
